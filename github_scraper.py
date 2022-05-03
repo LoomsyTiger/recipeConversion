ee20aff2a4b95373051e55884b16b3fcc0700a74
LoomsyTiger <juliusheenk@gmail.com>
2022-05-03 20:19:54 +0200

    'initial'

---
+++ github_scraper.py
@@ -0,0 +1,18 @@
+from recipe_scrapers import scrape_me
+
+# give the url as a string, it can be url from any site listed below
+scraper = scrape_me('')
+
+# Q: What if the recipe site I want to extract information from is not listed below?
+# A: You can give it a try with the wild_mode option! If there is Schema/Recipe available it will work just fine.
+# scraper = scrape_me('', wild_mode=True)
+
+scraper.title()
+scraper.total_time()
+scraper.yields()
+scraper.ingredients()
+scraper.instructions()
+scraper.image()
+scraper.host()
+scraper.links()
+scraper.nutrients()  # if available
