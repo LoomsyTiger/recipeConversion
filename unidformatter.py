138263aacd292c1abaf131114dc6be0d67f97818
LoomsyTiger <juliusheenk@gmail.com>
2022-05-31 18:27:20 +0200

    added unid formatter

---
+++ unidformatter.py
@@ -0,0 +1,11 @@
+def unid_formatter(unid):
+    eight = unid[0:8]
+    four1 = unid[8:12]
+    four2 = unid[12:16]
+    four3 = unid[16:20]
+    twelve = unid[20:]
+    delimiter = "-"
+    formatted = (eight+delimiter+four1+delimiter+four2+delimiter+four3+delimiter+twelve)
+    return formatted
+
+print(unid_formatter("12345678123412341234123456789123"))
