ca1de52731edecd91920a8e32e9e9e3df18d8133
LoomsyTiger <77097466+LoomsyTiger@users.noreply.github.com>
2022-06-02 21:24:16 +0200

    poging om iteratief dict bij te werken + opschonen

--- github_scraper.py
+++ github_scraper.py
@@ -4,13 +4,7 @@ from operator import index
 from recipe_scrapers import scrape_me
 from itertools import cycle
 
-# give the url as a string, it can be url from any site listed below
 scraper = scrape_me('https://www.bonappetit.com/recipe/spicy-braised-eggplant-noodles')
-
-# Q: What if the recipe site I want to extract information from is not listed below?
-# A: You can give it a try with the wild_mode option! If there is Schema/Recipe available it will work just fine.
-# scraper = scrape_me('', wild_mode=True)
-
 title = scraper.title()
 scraper.total_time()
 scraper.yields()
@@ -23,10 +17,10 @@ scraper.nutrients()  # if available
 
 # voor alle 'oz.', 'cup', 'inch (”)' en 'lb.'
 # vertaal het getal links hiervan naar de bijbehorende metric eenheid
-    # oz. = ml of gram
-    # cup = ml of gram>>
-    # lb. = gram
-    # ” = cm
+#     oz. = ml of gram
+#     cup = ml of gram>>
+#     lb. = gram
+#     ” = cm
 
 # key values
 unitMapping = {  
@@ -39,39 +33,30 @@ unitMapping = {
 key_list = list(unitMapping.keys())
 val_list = list(unitMapping.values())
 
-print(key_list, val_list)
-
 # vervang alle units uit de mapping
-for ingredient in allIngredients:
-    ingredientM = ingredient
-    
-    for unit in unitMapping.keys():
-        if unit in ingredient:
-            ingredientM = ingredientM.replace(unit, unitMapping.get(unit))
-    # print(ingredientM) 
-    
-# volgende functions nodig:
-    # gebruik unit als delimiter
-    # pak de laatste karakters uit de linker snippet
-    # vorm om naar integer
-    # reken de waarden om naar metric
-    
+class unit_replace:
+    for ingredient in allIngredients:
+        ingredientM = ingredient
+        
+        for unit in unitMapping.keys():
+            if unit in ingredient:
+                ingredientM = ingredientM.replace(unit, unitMapping.get(unit))
+        # print(ingredientM) 
+       
+# dict voor alle conversie-ratios
 conversionMapping = {
     "oz.": 28.34952
 }
 
+
+conversionOldNew = {}
+
 # split zin op spatie
-# loop tot integer
-# pak integer en index+1
-
-conversionOldNew = {
-    
-}
-
 for sentence in allIngredients:
     allWords = sentence.split(" ")
     print(sentence.split(" "))
     
+    # zet alle digits in conversionOldNew dict
     for word in allWords:
         if word.isdigit():
             enumerate(word)
@@ -83,12 +68,13 @@ for sentence in allIngredients:
         elif word == "¾":
             conversionOldNew.update({0.75:0})
 
-
+# return een lijst van alle keys uit een dict
 def getList(dict):
     return list(dict.keys())
 
 oldNewList = getList(conversionOldNew)
 
+# haal van alle keys in de dict het volgende woord op en werk de dict bij
 for word in allWords:
     for key in oldNewList:
         if key == word:
@@ -96,9 +82,7 @@ for word in allWords:
 
                 currWord = str(key)
                 nextWord = allWords[index+1]
+                conversionOldNew[currWord] = nextWord
                 
-                print(currWord, nextWord)
-
-
-
+                
 print(conversionOldNew)
