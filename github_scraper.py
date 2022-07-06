3cb3070c6d95702e49f6ce9acad415657c0c30fc
LoomsyTiger <77097466+LoomsyTiger@users.noreply.github.com>
2022-07-06 22:16:27 +0200

    volgorde aangepast omdat het door elkaar liep

--- github_scraper.py
+++ github_scraper.py
@@ -30,43 +30,17 @@ unitMapping = {
     "”": " cm"
 }
 
-key_list = list(unitMapping.keys())
-val_list = list(unitMapping.values())
+# nog niet gebruikt
+# key_list = list(unitMapping.keys())
+# val_list = list(unitMapping.values())
 
-# vervang alle units uit de mapping
-class unit_replace:
-    for ingredient in allIngredients:
-        ingredientM = ingredient
-        
-        for unit in unitMapping.keys():
-            if unit in ingredient:
-                ingredientM = ingredientM.replace(unit, unitMapping.get(unit))
-        # print(ingredientM) 
-       
 # dict voor alle conversie-ratios
 conversionMapping = {
-    "oz.": 28.34952
+    "gram": 28.34952  # oz naar gram
 }
 
 
 conversionOldNew = {}
-
-# split zin op spatie
-for sentence in allIngredients:
-    allWords = sentence.split(" ")
-    print(sentence.split(" "))
-    
-    # zet alle digits in conversionOldNew dict
-    for word in allWords:
-        if word.isdigit():
-            enumerate(word)
-            conversionOldNew.update({int(word):0})
-        elif word == "¼":
-            conversionOldNew.update({0.25:0})
-        elif word == "½":
-            conversionOldNew.update({0.5:0})
-        elif word == "¾":
-            conversionOldNew.update({0.75:0})
 
 # return een lijst van alle keys uit een dict
 def getList(dict):
@@ -75,14 +49,52 @@ def getList(dict):
 oldNewList = getList(conversionOldNew)
 
 # haal van alle keys in de dict het volgende woord op en werk de dict bij
+i = 0
+# split zin op spatie
+for sentence in allIngredients:
+    allWords = sentence.split(" ")
+    print(sentence.split(" "))
+    for word in allWords:
+        for key in oldNewList:
+            if key == word:
+                if (i+1 < len(allWords) and i -1 >= 0):
+                    currWord = str(key)
+                    nextWord = allWords[i+1]
+                    conversionOldNew[currWord] = nextWord
+        i+=1
+    
+# zet alle digits en fractions in conversionOldNew dict
 for word in allWords:
-    for key in oldNewList:
-        if key == word:
-            if (index+1 < len(allWords) and index -1 >= 0):
+    if word.isdigit():
+        int_word = int(word)
+        conversionOldNew.update({int_word:0})
+        # hoe zorg ik er voor dat deze vervolgens netjes de index +1 pakt?
+        conversionOldNew.update({int(word):0})
+    elif word == "¼":
+        conversionOldNew.update({0.25:0})
+    elif word == "½":
+        conversionOldNew.update({0.5:0})
+    elif word == "¾":
+        conversionOldNew.update({0.75:0})
+        
+            
+print(conversionOldNew)
+              
+                
+# vervangt de amerikaanse unit/keys in allIngredients met value uit de mapping
 
-                currWord = str(key)
-                nextWord = allWords[index+1]
-                conversionOldNew[currWord] = nextWord
-                
-                
+def imperial_metric(allIngredients):
+    ingredientsMetricList = []
+    for ingredient in allIngredients:
+        ingredientM = ingredient
+        
+        for unit in unitMapping.keys():
+            if unit in ingredient:
+                ingredientM = ingredientM.replace(unit, unitMapping.get(unit))
+                ingredientsMetricList.append(ingredientM)
+    return ingredientsMetricList
+
+ingredientsMetric = imperial_metric(allIngredients)
+print(ingredientsMetric)
+
 print(conversionOldNew)
