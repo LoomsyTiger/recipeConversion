7f97b2e729dfe45ba9af1b73981e800af37677d5
LoomsyTiger <juliusheenk@gmail.com>
2022-07-08 11:31:19 +0200

    more descriptive variable, dict and list names

--- github_scraper.py
+++ github_scraper.py
@@ -22,72 +22,72 @@ scraper.nutrients()  # if available
 #     lb. = gram
 #     ” = cm
 
-unitMapping = {  
+dict_unit_mapping = {  
     "oz.": "gram",
     "cup": "ml",
     "lb.": "gram",
     "”": " cm"
 }
 
-conversionMapping = {
+dict_conversion_rates = {
     "oz.": 28.34952,  # oz naar gram
     "cup": 235.6, # cups naar ml
     "lb.": 453.59237, # lb. naar gram
     "”": 2.54 # inch naar cm
 }
 
-conversionOldNew = {}
+imperial_ingredient_list = {} # dictionary met de integer-waarden van ieder ingrediënt als KEY + de string van de eenheid als VALUE
+oldNewList = [] # 
 
-# return een lijst van alle keys uit een dict
-def getList(dict):
-    return list(dict.keys())
-
-oldNewList = getList(conversionOldNew)
-
-# haal van alle keys in de dict het volgende woord op en werk de dict bij
 # split zin op spatie
 i = 0
 for sentence in all_ingredients:
     allWords = sentence.split(" ")
-    print(sentence.split(" "))
-    for word in allWords:
-        for key in oldNewList:
-            if key == word:
-                if (i+1 < len(allWords) and i -1 >= 0):
-                    currWord = str(key)
-                    nextWord = allWords[i+1]
-                    conversionOldNew[currWord] = nextWord
-        i+=1
-                
-print(conversionOldNew)
 
 # zet alle digits en fractions in conversionOldNew dict
 for word in allWords:
     if word.isdigit():
         int_word = int(word)
-        conversionOldNew.update({int_word:0})
+        imperial_ingredient_list.update({int_word:0})
         # hoe zorg ik er voor dat deze vervolgens netjes de index +1 pakt?
-        conversionOldNew.update({int(word):0})
+        imperial_ingredient_list.update({int(word):0})
     elif word == "¼":
-        conversionOldNew.update({0.25:0})
+        imperial_ingredient_list.update({0.25:0})
     elif word == "½":
-        conversionOldNew.update({0.5:0})
+        imperial_ingredient_list.update({0.5:0})
     elif word == "¾":
-        conversionOldNew.update({0.75:0})
+        imperial_ingredient_list.update({0.75:0})
+
+# return een lijst van alle keys uit een dict
+def getList(dict):
+    return list(dict.keys())
+
+oldNewList = getList(imperial_ingredient_list)
+
+# haal van alle keys in de dict het volgende woord op en werk de dict bij
+for word in allWords:
+    for key in oldNewList:
+        if key == word:
+            if (i+1 < len(allWords) and i -1 >= 0):
+                currWord = str(key)
+                nextWord = allWords[i+1]
+                imperial_ingredient_list[currWord] = nextWord
+    i+=1
+                
+print(imperial_ingredient_list)
 
 
-# vervangt de amerikaanse unit/keys in all_ingredients met value uit de mapping
+# vervangt de amerikaanse unitnamen in all_ingredients met value uit de mapping
 
 def imperial_metric(all_ingredients):
-    ingredientsMetricList = []
+    metric_ingredient_list = []
     for ingredient in all_ingredients:
         ingredientM = ingredient
         
         for unit in unitMapping.keys():
             if unit in ingredient:
                 ingredientM = ingredientM.replace(unit, unitMapping.get(unit))
-                ingredientsMetricList.append(ingredientM)
-    return ingredientsMetricList
+                metric_ingredient_list.append(ingredientM)
+    return metric_ingredient_list
 
 ingredientsMetric = imperial_metric(all_ingredients)
-print(ingredientsMetric)
